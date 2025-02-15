15638666822fcf13a57464aae7437b33af45ce7b
dinowork <dino.work@hotmail.com>
2025-02-14 23:21:12 -0300

    Configuração inicial

---
+++ src/test/java/br/com/dino/spring_boot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.dino.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
